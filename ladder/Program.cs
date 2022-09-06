d2cb56d36b9197c068ec510c103777d46dc42043
Trotil_2001 <trotil_2001@mail.ru>
2022-09-06 20:48:54 +0800

    Задача лесенка

---
+++ ladder/Program.cs
@@ -0,0 +1,15 @@
+﻿/*
+По данному натуральному n<=9 выведите лесенку из n ступенек,
+i-я ступенька состоить из чисел от 1 до i без пробелов.
+*/
+Console.Write("Введите число : ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n <= 9)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+            Console.Write(j);
+        Console.WriteLine();
+    }
+}
