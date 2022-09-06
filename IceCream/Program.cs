9891a2aac8014f8fd6639eadfdf7d66e7fe649af
Trotil_2001 <trotil_2001@mail.ru>
2022-09-06 20:48:03 +0800

    Задача мороженое

---
+++ IceCream/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+В кафе мороженное продают по три шарика и по пять шариков.
+Можно ли купить ровно k шариков мороженого?
+*/
+
+Console.Write("Введите число шариков мороженого: ");
+int k = Convert.ToInt32(Console.ReadLine());
+int a = 3;
+int b = 5;
+if (k > 0 )
+{
+    if (k % a == 0 || k % b == 0)
+        Console.Write("YES");
+    else
+        Console.Write("NO");
+}
+else
+    Console.Write("Введено не допустимое число!");
